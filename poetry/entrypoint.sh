70164adca5827f19e18a29549613998a4b122e32
Jack O'Neill <168836329+jackoneill77@users.noreply.github.com>
2025-05-24 20:13:19 +0200

    fix: docker uid

--- poetry/entrypoint.sh
+++ poetry/entrypoint.sh
@@ -5,9 +5,8 @@ cd /home/container
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-# Print current Poetry and Python version
+# Print current Poetry version
 poetry --version
-poetry run python --version
 
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
